f27dc427c4ef80df4649eb1eecaf4166da29fa07
sanjeevkoshti <koshtisanjeev6@gmail.com>
2026-02-16 19:11:14 +0530

    props using array of objects

--- src/Propss.jsx
+++ src/Propss.jsx
@@ -1,32 +1,53 @@
 import Product from "./Product";
-//individual props
-const product1 = {
-  title: "Mobile", 
-  price: 10000
-}
-const product2 = {
-  title: "Laptop", 
-  price: 40000
-}
-const product3 = {
-  title: "Tablet", 
-  price: 30000
-}
 
+//array of objects
+const products = [
+  { title: "Mobile", price: 10000 },
+  { title: "Laptop", price: 40000 },
+  { title: "Tablet", price: 30000 }
+];
+
+//display array of objects
 const Propss = () => {
   return (
     <>
-      <Product title={product1.title} price={product1.price}/>
-      <Product title={product2.title} price={product2.price}/>
-      <Product title={product3.title} price={product3.price}/>
-
-      {/* children props
-      <Product title={product3.title} price={product3.price}>
-        <p>This is a book about Express</p>
-      </Product> */}
+      {products.map((product) => (
+        <Product title={product.title} price={product.price}/>
+      ))}
     </>
   );
 }
+
+//individual Objects
+// const product1 = {
+//   title: "Mobile", 
+//   price: 10000
+// }
+// const product2 = {
+//   title: "Laptop", 
+//   price: 40000
+// }
+// const product3 = {
+//   title: "Tablet", 
+//   price: 30000
+// }
+
+//desplay individual objects
+// const Propss = () => {
+//   return (
+//     <>
+//       <Product title={product1.title} price={product1.price}/>
+//       <Product title={product2.title} price={product2.price}/>
+//       <Product title={product3.title} price={product3.price}/>
+
+//       {/* children props */}
+//       <Product title={product3.title} price={product3.price}>
+//         <p>This is a book about Express</p>
+//       </Product>
+//     </>
+//   );
+// }
+
 
 //using hard coded props with chidrens
 // function Propss() {
