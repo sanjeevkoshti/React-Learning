cacfbbfb3d611027afa623c126a6bd31b5c16a05
sanjeevkoshti <koshtisanjeev6@gmail.com>
2026-02-18 16:44:16 +0530

    add delete button to Todo List

--- src/states/TodoList.jsx
+++ src/states/TodoList.jsx
@@ -7,6 +7,9 @@ export default function TodoList(){
         setTodos([...todos, newTodo]);
         setNewTodo("");
     }
+    const deleteTodo=(index)=>{
+        setTodos(todos.filter((todos, idx)=>idx!=index));
+    }
     return(
         <div style={styles}>
             <h1>Todo List</h1>
@@ -14,7 +17,10 @@ export default function TodoList(){
             <button onClick={addTodo} style={{padding:"0.5rem 1rem", fontSize:"1.2rem", fontWeight:"bold", borderRadius:"5px", cursor:"pointer"}}>Add Todo</button>
             <ul style={{padding:"0", margin:"0", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"start", backgroundColor:"#ffd2d2ff", borderRadius:"10px", padding:"1rem", marginTop:"1rem"}}>
                 {todos.map((todo, index)=>(
-                    <li style={{fontSize:"2rem", margin:"0.5rem 0 0 1.5rem"}} key={index}>{todo}</li>
+                    <li style={{listStyleType:"none", fontSize:"2rem", margin:"0.5rem 0 0 0", display:"flex", justifyContent:"center", alignItems:"center", gap:"1rem"}} key={index}>
+                        <button onClick={()=>deleteTodo(index)} style={{padding:"0", fontSize:"1.4rem", fontWeight:"bold", borderRadius:"50%", cursor:"pointer", border:"none", backgroundColor:"#ffd2d2ff"}}><i className="fa-solid fa-trash"></i></button>
+                        <span>{todo}</span>
+                    </li>
                 ))}
             </ul>
         </div>
