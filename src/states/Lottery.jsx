3ac0b7240e5516ac29f83dbe0d41b094442fbbe8
sanjeevkoshti <koshtisanjeev6@gmail.com>
2026-02-20 12:42:53 +0530

    update lottery ticket component

--- src/states/Lottery.jsx
+++ src/states/Lottery.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./Lottery.css";
+import { genTicket, sum } from "./LotteryFun.js"
 
 export default function Lottery(){
-    let [ticket, setTicket] = useState([0,0,0]);
+    let [ticket, setTicket] = useState(genTicket(3));
+    let isWinning = sum(ticket) === 15;
+    let buyTicket=()=>{
+        setTicket(genTicket(3));
+    }
     return(
         <div className="lottery">
             <h1>Lottery Game!</h1>
@@ -11,6 +16,8 @@ export default function Lottery(){
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
+            <button className="buyBtn" onClick={buyTicket}>Buy New Ticket</button>
+            <h2 style={{color: "green", fontSize: "1.8rem"}}>{isWinning && "Congratulations, you won! 🎉"}</h2>
         </div>
     );
 }
