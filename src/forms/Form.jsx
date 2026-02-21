a03600e3f53be9d162fb2c2b6199a9c6ada28b65
sanjeevkoshti <koshtisanjeev6@gmail.com>
2026-02-21 19:44:00 +0530

    create multiple inputs handle with single function

--- src/forms/Form.jsx
+++ src/forms/Form.jsx
@@ -1,38 +1,74 @@
 import { useState } from "react";
 
 export default function Form() {
-    let [fullName, setFullName] = useState("");
-    let [userName, setUserName] = useState("");
 
-    let handleFullName = (e) => {
-        setFullName(e.target.value);
+    let [formData, setFormData] = useState({
+        fullName: "",
+        userName: "",
+        password: "",
+    })
+    let handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
     }
-    let handleUserName = (e) => {
-        setUserName(e.target.value);
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        setFormData({
+            fullName: "",
+            userName: "",
+            password: "",
+        })
+
     }
+
+    // let [fullName, setFullName] = useState("");
+    // let [userName, setUserName] = useState("");
+
+    // let handleFullName = (e) => {
+    //     setFullName(e.target.value);
+    // }
+    // let handleUserName = (e) => {
+    //     setUserName(e.target.value);
+    // }
     return (
         <>
-            <form style={formStyle}>
+            <form style={formStyle} onSubmit={handleSubmit}>
                 <div>
                 <label htmlFor="fullName" style={{ fontSize: "1.8rem", color: "white" }}>Full Name : </label>
                 <input
                     type="text"
                     placeholder="Enter full name"
-                    value={fullName}
-                    onChange={handleFullName}
+                    value={formData.fullName}
+                    onChange={handleChange}
                     id="fullName"
+                    name="fullName"
                     style={{ fontSize: "1.5rem" }}
                 />
                 </div>
-                
                 <div>
                 <label htmlFor="userName" style={{ fontSize: "1.8rem", color: "white" }}>Username : </label>
                 <input
                     type="text"
                     placeholder="Enter username"
-                    value={userName}
-                    onChange={handleUserName}
+                    value={formData.userName}
+                    onChange={handleChange}
                     id="userName"
+                    name="userName"
+                    style={{ fontSize: "1.5rem" }}
+                />
+                </div>
+                <div>
+                <label htmlFor="password" style={{ fontSize: "1.8rem", color: "white" }}>Password : </label>
+                <input
+                    type="password"
+                    placeholder="Enter password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    id="password"
+                    name="password"
                     style={{ fontSize: "1.5rem" }}
                 />
                 </div>
