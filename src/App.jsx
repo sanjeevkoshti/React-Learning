4ec989f9b5068ad3a355e48a1888581c17fc59aa
sanjeevkoshti <koshtisanjeev6@gmail.com>
2026-02-22 18:56:48 +0530

    setup the weather App folder and component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Propss from './props/Propss.jsx';
 import States from './states/States.jsx';
 import Forms from './forms/Forms.jsx';
+import Weather from './weatherApp/Weather.jsx';
 
 
 function App() {
   return (
     <div className='app'>
-      <Forms/>
+      <Weather/>
+      {/* <Forms/> */}
       {/* <States/> */}
       {/* <Propss/> */}
     </div>
