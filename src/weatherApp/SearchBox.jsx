11e151a48e2a2c8e0723a6ec167b813e7b7dd696
sanjeevkoshti <koshtisanjeev6@gmail.com>
2026-02-23 17:10:49 +0530

    setup the project with material ui dependencies

--- src/weatherApp/SearchBox.jsx
+++ src/weatherApp/SearchBox.jsx
@@ -1,15 +1,17 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './SearchBox.css';
+
 export default function SearchBox(){
     return(
-        <div>
-            <input type="text" placeholder="Search..." style={style}/>
+        <div className='searchBox'>
+            <h3>Search for the weather</h3>
+            <form className='form'>
+                <TextField id="city" label="Search City" variant="outlined" required/>
+                <Button variant="contained" type="submit">
+                    Search
+                </Button>
+            </form>
         </div>
     );
-}
-
-let style = {
-    fontSize: "1.3rem",
-    padding: "0.5rem",
-    borderRadius: "5px",
-    border: "1px solid #ccc",
-    outline: "none",
 }
